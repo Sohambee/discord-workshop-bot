7bd05b5add06989e91e9bd55d437e416d0b63760
Soham Bhavsar <soham.bhavsar@hotmail.com>
2022-02-18 02:13:38 -0500

    first commit

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV ==='production') {
-    module.exports = {TOKEN:process.env.TOKEN}
-}else{
-    module.exports = {TOKEN:"OTQ0MDI0NjQ2NjA3NjAxNjg0.Yg7ldg.dON_cZkOiTuVpQN-pnUTetfKCwo"}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {TOKEN:process.env.TOKEN, YOUTUBE:process.env.YOUTUBE}
+} else {
+    module.exports = {TOKEN:"OTQ0MDc2MzAyNTI3MzkzODEz.Yg8Vkg.A2Mv9lhRta1VCd-5pBFUmN9fE6w", YOUTUBE: "AIzaSyDlGG_bNw8jggtuYDXF0vOptGPBiTEFv0M"}
 } 
+
